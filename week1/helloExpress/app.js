7ac6fba65862e078a080905c79bf5a80aa3c7f1b
sosana <thesosana@gmail.com>
2014-06-26 18:29:47 +0000

    pushing updates to express/swig/mongo in helloExpress

--- week1/helloExpress/app.js
+++ week1/helloExpress/app.js
@@ -6,15 +6,34 @@
 * To change this template use Tools | Templates.
 */
 var express = require('express'),
-    app = express();
+    app = express(),
+	cons = require('consolidate'),
+	MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server;
+
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+
+var mongoclient = new MongoClient(new Server('localhost', 27017,
+                                             { 'native_parser' : true }));
+var db = mongoclient.db('course');
 
 app.get('/', function (req, res){
-    res.send("Hello, Beautiful World!");
+    
+    db.collection('course').findOne({}, function (err, doc) {
+    	res.render('hello', doc);                                           
+    });
 });
 
 app.get('*', function (req, res){
     res.send("Page Not Found YO!", 404);
 });
 
-app.listen(8080);
-console.log("Express server started on port 8080 Yo!");
+mongoclient.open(function (err, mongoclient){
+	
+    if (err) throw err;
+    
+    app.listen(8080);
+    console.log("Express server started on port 8080 Yo!"); 
+});
