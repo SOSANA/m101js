d22a41a31e16bda361e7948762c196172a3b92a5
sosana <thesosana@gmail.com>
2014-11-13 19:56:35 +0000

    updating week7 models/middleware/views

--- lesson examples/week7/routes/index.js
+++ lesson examples/week7/routes/index.js
@@ -1,7 +1,12 @@
+var errors = require('./errors');
+
 module.exports = function (app) {
     
-    //home page
+    // home page
     app.get('/', function (req, res) {
         res.render('home.jade');
     })
+    
+    // error handlers
+    errors(app);
 }
